6147580d934874412b506388d4db66b1a71c0e94
Selvanayagam Amburose <ambroseselva001@gmail.com>
2026-09-17 17:33:05 +0530

    fix(analytics): remove duplicate Meta Pixel initialization and PageView from AIMasterclassPage

--- src/components/ai_masterclass/AIMasterclassPage.tsx
+++ src/components/ai_masterclass/AIMasterclassPage.tsx
@@ -14,35 +14,16 @@ import SocialProofToast from "../common/SocialProofToast";
 
 const AIMasterclassPage: React.FC = () => {
     useEffect(() => {
-        const script = document.createElement("script");
-        script.id = "meta-pixel-aimasterclass";
-        script.innerHTML = `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2155724311994078');
-            fbq('track', 'PageView');
-            fbq('track', 'ViewContent');
-        `;
         document.title = "AI Secrets Revealed & Business Automation Masterclass | Tech Tycoon";
-        document.head.appendChild(script);
-        
-        return () => {
-            const el = document.getElementById("meta-pixel-aimasterclass");
-            if (el) el.remove();
-        };
+        if (typeof (window as any).fbq === 'function') {
+            (window as any).fbq('track', 'ViewContent', {
+                content_name: 'AI Secrets Revealed'
+            });
+        }
     }, []);
 
     return (
         <AIMasterclassRegisterModalProvider>
-            <noscript>
-                <img height="1" width="1" style={{display: "none"}} src="https://www.facebook.com/tr?id=2155724311994078&ev=PageView&noscript=1" alt="" />
-            </noscript>
             <div className="min-h-screen bg-white text-slate-900 font-sans overflow-x-hidden relative">
                 <AIMasterclassNavbar />
                 <AIMasterclassHeroSection />
